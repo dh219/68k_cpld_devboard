68dd23ff930217e96a1ff03274bc588c0d4b9cfe
David <dh219@aibs.org.uk>
2024-10-15 22:13:35 +0100

    Basic address-driven transfer working, but no speed optimisation

--- rtl/picovid/picovid.v
+++ rtl/picovid/picovid.v
@@ -31,9 +31,9 @@ module picovid (
 	
 	input TP1,
 	
-	output P50, // RTS
+	input P50, // POLL ADDRESS
 	
-	input P52,
+	input P52, // POLL ADDRESS
 	input P53,
 	input P54,
 	input P55,
@@ -44,77 +44,99 @@ module picovid (
 	input P60,
 	input P61,
 	
-	output P63,
-	output P64,
-	output P65,
-	output P66,
-	output P67,
-	output P68,
+	output P63, // DATA OUT
+	output P64, // DATA OUT
+	output P65, // DATA OUT
+	output P66, // DATA OUT
+	output P67, // DATA OUT
+	output P68, // DATA OUT
 	
-	output P70,
-	output P71,
-	input P72,
-	input P73	// strobe
+	output P70, // DATA OUT 
+	output P71, // DATA OUT
+	output P72, 	// RTS (out)
+	input P73 	// POLL ADDRESS
     );
 
 reg [23:0] a_in;
 reg [15:0] d_in;
 
 reg [7:0] d = 'd1;
-reg rts = 1'b1;
-reg [3:0] strobestate = 'd0;
+reg _rts = 1'b1;
+//reg [3:0] strobestate = 'd0;
+
+//wire rst = ( A[23:20] == 'h2 );
+wire [2:0] padd = { P52, P50, P73 };
 
 
-wire write = ( {A[23:15]} == 9'b000001111 ) && ~RW && ~DTACK;
-//wire write = ( {A[23:15]} == { 4'h6, 4'h0, 1'b0 } ) && ~RW && ( ~DTACK );
-wire ack = ( strobestate == 'd5 );
+//wire write = ( {A[23:15]} == 9'b000001111 ) && ~RW && ~DTACK;
+wire write = ( A[23:20] == { 4'h3 } ) && ~RW && ( ~DTACK );
+//wire ack = ~_rts & (padd != padd_rts);
+wire ack = ~_rts & (padd == 'd0);
+
 
 always @( posedge write or posedge ack ) begin
 	if( ack )
-		rts <= 1'b1;
-	else if( write & rts ) begin
+		_rts <= 1'b1;
+	else if( write & _rts) begin
 		d_in <= D[15:0];
 		a_in <= {A[23:1],1'b0};
-		rts <= 1'b0;
+		_rts <= 1'b0;
 	end		
 end
-
+/*
 wire strobe = P73;
 
 
-always @(negedge strobe ) begin
+always @(negedge strobe or posedge rst ) begin
 //	d_in <= D[15:0];
 //	a_in <= {A[23:1],1'b0};
-	case( strobestate )
-		'd0: begin
-			d <= a_in[23:16];
-			strobestate <= 'd1;
-		end
-		'd1: begin
-			d <= a_in[15:8];
-			strobestate <= 'd2;
-		end
-		'd2: begin
-			d <= a_in[7:0];
-			strobestate <= 'd3;			
-		end
-		'd3: begin
-			d <= d_in[15:8];
-			strobestate <= 'd4;
-		end
-		'd4: begin
-			d <= d_in[7:0];
-			strobestate <= 'd5;
-		end
-		'd5: begin // blank strobe to end
-			strobestate <= 'd0;
-		end
-	endcase
-		
+
+	if( rst )
+		strobestate <= 'd0;
+	else begin
+		case( strobestate )
+			'd0: begin
+				d <= a_in[23:16];
+				strobestate <= 'd1;
+			end
+			'd1: begin
+				d <= a_in[15:8];
+				strobestate <= 'd2;
+			end
+			'd2: begin
+				d <= a_in[7:0];
+				strobestate <= 'd3;			
+			end
+			'd3: begin
+				d <= d_in[15:8];
+				strobestate <= 'd4;
+			end
+			'd4: begin
+				d <= d_in[7:0];
+				strobestate <= 'd5;
+			end
+			'd5: begin // blank strobe to end
+				strobestate <= 'd0;
+			end
+		endcase
+	end
 end
+*/
+
+always @( padd ) begin
+	case(padd)
+		'd0: d <= a_in[23:16];
+		'd1: d <= a_in[15:8];
+		'd2: d <= a_in[7:0];
+		'd3: d <= d_in[15:8];
+		'd4: d <= d_in[7:0];
+		default: 	d <= { 5'd0, padd };
+	endcase
+end
+	
 
 
-wire oe = (strobestate == 'd0);
+wire oe = ( padd == 'd7 );
 
 // data lines
 assign P63 = oe ? 1'bz : d[0];
@@ -126,6 +148,6 @@ assign P68 = oe ? 1'bz : d[5];
 assign P70 = oe ? 1'bz : d[6];
 assign P71 = oe ? 1'bz : d[7];
 
-assign P50 = rts ? 1'bz: 1'b0;
+assign P72 = _rts ? 1'bz: 1'b0;
 
 endmodule
