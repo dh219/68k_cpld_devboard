46287f6fdb232dbec3277d19e97c7f339fdaed66
David <dh219@aibs.org.uk>
2025-10-01 21:47:50 +0100

    Trying hard to get normal write-sniffing to work, but corrupt transfers.

--- rtl/picovid/picovid.v
+++ rtl/picovid/picovid.v
@@ -50,27 +50,24 @@ reg [15:0] d1;
 reg [7:0] d_out = 'd1;
 
 reg displaymode = 1'b1;
-reg capturemode = 1'b1;
-
-reg [15:0] scrcounter = 'd0;
+reg [18:0] scrcounter = 'd0;
 
 //wire [2:0] padd = { P52, P50, P73 };
 
 //wire base = ( A[18:15] == 4'b1111 );
 
-reg address_trigger;
 reg uds0;
 reg lds0;
 reg uds1;
 reg lds1;
 wire idle;
+wire LOAD = TP1;
 
-always @( negedge CLK ) begin
+reg address_trigger;
+always @( posedge OSC48 ) begin
 	address_trigger <= 1'b0;
-	if( ~RW & ~AS & ~(UDS&LDS) && ~TP4 && idle ) begin
+	if( ~RW & ~AS & ~(UDS&LDS) && LOAD && idle ) begin
 		address_trigger <= 1'b1;
-		uds1 <= UDS;
-		lds1 <= LDS;
 	end
 end
 
@@ -86,7 +83,9 @@ reg [7:0] vsync_counter = 'd0;
 
 always @( posedge address_trigger ) begin
 	d1 <= D[15:0];
-	a1 <= {A[23:1],1'b0};					
+   a1 <= {A[23:1],1'b0};					
+	uds1 <= UDS;
+	lds1 <= LDS;
 	trig1 <= ~trig1;
 end
 
@@ -95,14 +94,13 @@ reg [3:0] cycle = 'd0;
 reg active = 1'b0;
 reg [2:0] type;
 assign idle = cycle == 'd0; // perhaps duplicating active?
-wire LOAD = TP1;
 
 always @( posedge LOAD or negedge VSYNC ) begin
 
 	scrcounter <= scrcounter + 'd2;
 	
 	if( !VSYNC )
-		scrcounter <= 'd0;
+		scrcounter <= 'h78000;
 	else 
 	begin
 		if( idle ) begin // if a transmission is already in play, we'll have to skip this one.
@@ -119,19 +117,24 @@ end
 reg uds_composite = 'd1;
 reg lds_composite = 'd1;
 
+reg cmode = 1'b1;
 always @(posedge OSC48 ) begin
 	case(cycle)
 		'd0: begin
-			if( trig0 ^ ack0 ) begin
-				uds_composite <= uds0;
-				lds_composite <= lds0;
-				cycle <= 'd1;
-			end
-			else if( trig1 ^ ack1 ) begin
+			if( trig1 ^ ack1 ) begin
+				cmode <= 1'b1;
 				uds_composite <= uds1;
 				lds_composite <= lds1;
 				cycle <= 'd1;
 			end
+			/*
+			if( trig0 ^ ack0 ) begin
+				cmode <= 1'b0;
+				uds_composite <= uds0;
+				lds_composite <= lds0;
+				cycle <= 'd1;
+			end
+			*/
 			clkout <= 1'b0;
 			active <= 1'b0;
 			_dtack_in <= 1'b1;
@@ -140,35 +143,35 @@ always @(posedge OSC48 ) begin
 		'd1: begin
 			ack0 <= trig0;
 			ack1 <= trig1;
-			d_out <= capturemode ? a1[23:16] : a0[23:16];
+			d_out <= cmode ? a1[23:16] : a0[23:16];
 			type <= 'd1;
 			active <= 1'b1;
 			clkout <= 1'b0;
 			cycle <= 'd2;			
 		end
 		'd3: begin
-			d_out <= capturemode ? a1[15:8] : a0[15:8];
+			d_out <= cmode ? a1[15:8] : a0[15:8];
 			type <= 'd2;
 			active <= 1'b1;
 			clkout <= 1'b0;
 			cycle <= 'd4;			
 		end
 		'd5: begin
-			d_out <= capturemode ? a1[7:0] : a0[7:0];
+			d_out <= cmode ? a1[7:0] : a0[7:0];
 			type <= 'd3;
 			active <= 1'b1;
 			clkout <= 1'b0;
 			cycle <= 'd6;
 		end
 		'd7:  begin
-			d_out <= capturemode ? d1[15:8] : d0[15:8];
+			d_out <= cmode ? d1[15:8] : d0[15:8];
 			type <= lds_composite ? 'd5 : 'd4; // if LDS is coming type = 4, else let's finish with type = 5
 			active <= 1'b1;
 			clkout <= uds_composite ? 1'b1 : 1'b0;
 			cycle <= 'd8;
 		end
 		'd9: begin
-			d_out <= capturemode ? d1[7:0] : d0[7:0];
+			d_out <= cmode ? d1[7:0] : d0[7:0];
 			type <= 'd6;
 			active <= 1'b1;
 			clkout <= lds_composite ? 1'b1 : 1'b0;
@@ -216,22 +219,23 @@ wire falpal_reg_access = ( A[23:10] == 14'h3fe6 ) && !UDS && !LDS && !AS; // Fal
 assign DTACK = (reg_access|falpal_reg_access|altreg_access) ? 1'b0 : 1'bz;
 
 /* reset held timer (using vsync) to switch modes */
+/* can't fit
 always @( negedge VSYNC ) begin
 	if( !RESET )
 		vsync_counter <= vsync_counter + 'd1;
 	else
 		vsync_counter <= 'd0;
 end
-/*
+
 wire modereg = altreg_access & ( A[3:1] == 3'd7 ) & ~RW & address;
 wire modechange = ( vsync_counter[7] | modereg );
 always @( posedge modechange ) begin
-	mode <= ~mode;
+	displaymode <= ~displaymode;
 end
 */
 
 always @(posedge OSC48 ) begin
-	capturemode <= TP2; // normally displaymode
+	displaymode <= TP2; // normally displaymode
 end
 
 assign TP4 = VSYNC;
